e0478eaed6c258c0a4f6808cccf2934e63087210
Akshata Deshpande <ad.march6@gmail.com>
2020-10-15 17:43:20 +0100

    Removed old code , use of DI instead.

--- app/src/main/java/com/example/newsapplication/network/NewsApiService.kt
+++ app/src/main/java/com/example/newsapplication/network/NewsApiService.kt
@@ -1,20 +1,19 @@
 package com.example.newsapplication.network
 
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val BASE_URL = "http://newsapi.org/v2/"
+public const val BASE_URL = "http://newsapi.org/v2/"
 
 enum class NewsAPIStatus { LOADING, ERROR, DONE }
 
 //Use of Retrofit and Moshi library to parse incoming JSON response.
 
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create())
-    .baseUrl(BASE_URL)
-    .build()
+// No need as dependency is being used.
+//private val retrofit = Retrofit.Builder()
+//    .addConverterFactory(MoshiConverterFactory.create())
+//    .baseUrl(BASE_URL)
+//    .build()
 
 interface NewsApiService {
     @GET("everything")
@@ -27,9 +26,10 @@ interface NewsApiService {
     ): ResponseData
 }
 
+// No need as dependency is being used.
 // Retrofit instance
-object NewsApi {
-    val retrofitService: NewsApiService by lazy {
-        retrofit.create(NewsApiService::class.java)
-    }
-}
+//object NewsApi {
+//    val retrofitService: NewsApiService by lazy {
+//        retrofit.create(NewsApiService::class.java)
+//    }
+//}
